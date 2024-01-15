8c847932b8c334e524c3996f125d1529c87dc1a7
Supakit <book0970403815@hotmail.com>
2024-01-15 10:21:31 +0700

    feat : send assignment by book

--- server/apps/products.js
+++ server/apps/products.js
@@ -1,15 +1,83 @@
 import { Router } from "express";
+import { ObjectId } from "mongodb";
+import { db } from "../utils/db.js";
 
 const productRouter = Router();
 
-productRouter.get("/", (req, res) => {});
+productRouter.get("/", async (req, res) => {
+  const collection = db.collection("Products");
+  const products = await collection.find({}).limit(10).toArray();
+  return res.json({
+    data: products,
+  });
+});
 
-productRouter.get("/:id", (req, res) => {});
+productRouter.get("/:id", async (req, res) => {
+  //   const collection = db.collection("Product");
+  //   const productId = new ObjectId(req.params.id);
+  //   const productById = await collection.find({ _id: productId }).toArray();
+  //   return res.json({
+  //     data: productById,
+  //   });
+  const collection = db.collection("Products");
+  const productId = new ObjectId(req.params.id);
 
-productRouter.post("/", (req, res) => {});
+  const productById = await collection.findOne({ _id: productId });
 
-productRouter.put("/:id", (req, res) => {});
+  return res.json({ data: productById });
+});
 
-productRouter.delete("/:id", (req, res) => {});
+productRouter.post("/", async (req, res) => {
+  const collection = db.collection("Products");
+  const productData = { ...req.body };
+  const products = await collection.insertOne(productData);
+  return res.json({
+    message: "Product has been created successfully",
+  });
+});
+
+// productRouter.put("/:id", async (req, res) => {
+//   const collection = db.collection("Product");
+//   const productId = new ObjectId(req.params.productId);
+//   const newProductData = { ...req.body, created_at: new Date() };
+//   await collection.updateOne(
+//     {
+//       _id: productId,
+//     },
+//     {
+//       $set: newProductData,
+//     }
+//   );
+//   return res.json({
+//     message: "Product has been updated successfully",
+//   });
+// });
+
+productRouter.put("/", async (req, res) => {
+  const collection = db.collection("Products");
+  const productId = new ObjectId(req.params.id);
+  const productData = { ...req.body };
+  await collection.updateOne(
+    {
+      _id: productId,
+    },
+    {
+      $set: productData,
+    }
+  );
+
+  return res.json({
+    message: `Product has been updated successfully`,
+  });
+});
+
+productRouter.delete("/:id", async (req, res) => {
+  const collection = db.collection("Products");
+  const productId = new ObjectId(req.params.id);
+  await collection.deleteOne({ _id: productId });
+  return res.json({
+    message: "Product has been deleted successfully",
+  });
+});
 
 export default productRouter;
--- server/utils/db.js
+++ server/utils/db.js
@@ -1,1 +1,10 @@
 // Set up db connection here
+import { MongoClient } from "mongodb";
+
+// MongoDB จะมี Url ให้เราทำการเชื่อมต่อ
+// โดยปกติแล้ว Url จะอยู๋ในรูปแบบ `mongodb://url:port`
+const connectionString = "mongodb://127.0.0.1:27017";
+
+export const client = new MongoClient(connectionString);
+
+export const db = client.db("practice-mongo");
